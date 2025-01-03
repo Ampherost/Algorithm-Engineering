f2f89d1f24c795bb4e4b1fc9e149271867550939
Ampherost <Ampherost235@gmail.com>
2025-01-03 06:08:43 -0800

    Flag setup pt2

--- Custom_Sqrt/sqrt.cpp
+++ Custom_Sqrt/sqrt.cpp
@@ -2,11 +2,20 @@
 #include <chrono>
 #include <cmath>
 
-
-
+#define NEWTON_RAPHSON
+// #define BINARY_SEARCH
+// #define POLYNOMIAL
 
 
 extern "C" void fastsqrt(const float* in, float* out, unsigned count) {
 // Newton-Raphson iteration
+#ifdef NEWTON_RAPHSON
 
+// Binary Search Method
+#elif defined(BINARY_SEARCH)
+
+// Polynomial Method
+#elif defined(POLYNOMIAL)
+
+#endif
 }
