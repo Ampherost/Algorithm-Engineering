a62ee1077c464f67ee9ae964dcfb18891d2dc65c
Ampherost <Ampherost235@gmail.com>
2025-01-04 09:42:01 -0800

    newton raphson v1

--- Custom_Sqrt/sqrt.cpp
+++ Custom_Sqrt/sqrt.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <chrono>
 #include <cmath>
+#include <fstream>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
 
 #define NEWTON_RAPHSON
 // #define BINARY_SEARCH
@@ -10,6 +15,22 @@
 extern "C" void fastsqrt(const float* in, float* out, unsigned count) {
 // Newton-Raphson iteration
 #ifdef NEWTON_RAPHSON
+for (unsigned i = 0; i < count; ++i) {
+        float x = in[i];
+        float y = x / 2.0f;  // Initial guess
+        const float tolerance = 1e-6;  // Precision tolerance
+        
+        // Iteratively improve the guess
+        while (true) {
+            float nextY = 0.5f * (y + x / y);
+            if (fabs(nextY - y) < tolerance) {
+                break;
+            }
+            y = nextY;
+        }
+        
+        out[i] = y;  // Store the result
+    }
 
 // Binary Search Method
 #elif defined(BINARY_SEARCH)
@@ -18,4 +39,9 @@ extern "C" void fastsqrt(const float* in, float* out, unsigned count) {
 #elif defined(POLYNOMIAL)
 
 #endif
+}
+
+int main() {
+   
+    return 0;
 }
