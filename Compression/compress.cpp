4f9231f05f6fb7a3aa090f59d7f43b5e35f51b34
Ampherost <Ampherost235@gmail.com>
2025-01-11 20:03:55 -0800

    Added data structure to track matches

--- Compression/compress.cpp
+++ Compression/compress.cpp
@@ -4,6 +4,14 @@
 #include <algorithm>  
 #include <iostream>
 
+static const int WINDOW_SIZE = 4096;    
+static const int MIN_MATCH_LENGTH = 3; 
+
+struct Match {
+    int offset;
+    int length;
+};
+
 std::string compress(const std::string& source) {}
 
 std::string decompress(const std::string& source) {}
