a89a1ff15dac063178f58b2caac186b13b2aa91e
Ampherost <Ampherost235@gmail.com>
2025-02-11 11:36:36 -0800

    12 common passwords

---
+++ Password_Guesser/guesser.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+std::vector<std::string> guesses(unsigned known) {
+  std::vector<std::string> response {
+   "000000",
+   "badbad",
+   "111111",
+   "222222",
+   "333333",
+   "444444",
+   "555555",
+   "666666",
+   "777777",
+   "888888",
+   "999999",
+   "123456",
+   "654321",
+  };
+  
+  return response;
+}
